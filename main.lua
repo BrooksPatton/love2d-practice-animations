c3ba4cc8badded845fda0e9a8c27268e0fa69979
Brooks Patton <brooks@brookzerker.com>
2016-11-20 21:10:34 -0700

    the ship can now change speeds!

--- main.lua
+++ main.lua
@@ -10,6 +10,22 @@ function love.load()
     radians = 0,
     travelRadius = 100
   }
+
+  buttonSpeedUp = {
+    x = 5,
+    y = 25,
+    color = {249, 203, 79},
+    width = 20,
+    height = 20
+  }
+
+  buttonSpeedDown = {
+    x = 30,
+    y = 25,
+    color = {29, 141, 250},
+    width = 20,
+    height = 20
+  }
 end
 
 function love.update(dt)
@@ -26,6 +42,13 @@ function love.draw()
   love.graphics.setBackgroundColor(backgroundColor[1], backgroundColor[2], backgroundColor[3])
   love.graphics.draw(warship, warshipLocation.x, warshipLocation.y, warshipLocation.radians, 1, 1, 16, 16)
 
+  love.graphics.print('speed ' .. warshipLocation.travelRadius, 5, 5)
+
+  love.graphics.setColor(unpack(buttonSpeedUp.color))
+  love.graphics.rectangle('fill', buttonSpeedUp.x, buttonSpeedUp.y, buttonSpeedUp.width, buttonSpeedUp.height)
+
+  love.graphics.setColor(unpack(buttonSpeedDown.color))
+  love.graphics.rectangle('fill', buttonSpeedDown.x, buttonSpeedDown.y, buttonSpeedDown.width, buttonSpeedDown.height)
 end
 
 function warshipMoveFinished()
@@ -44,8 +67,26 @@ function isClickedWithinCircle(x, y)
   return warshipLocation.travelRadius - distanceFromShip > 0
 end
 
+function isSpeedUpButtonPressed(x, y)
+  return buttonSpeedUp.x < x and
+  buttonSpeedUp.x + buttonSpeedUp.width > x and
+  buttonSpeedUp.y < y and
+  buttonSpeedUp.y + buttonSpeedUp.height > y
+end
+
+function isSpeedDownButtonPressed(x, y)
+  return buttonSpeedDown.x < x and
+  buttonSpeedDown.x + buttonSpeedDown.width > x and
+  buttonSpeedDown.y < y and
+  buttonSpeedDown.y + buttonSpeedDown.height > y
+end
+
 function love.mousepressed(x, y, button, isTouch)
-  if not warshipLocation.moving and isClickedWithinCircle(x, y) then
+  if isSpeedUpButtonPressed(x, y) then
+    warshipLocation.travelRadius = warshipLocation.travelRadius + 10
+  elseif isSpeedDownButtonPressed(x, y) then
+    warshipLocation.travelRadius = warshipLocation.travelRadius - 10
+  elseif not warshipLocation.moving and isClickedWithinCircle(x, y) then
     flux.to(warshipLocation, 0.5, {radians = calculateRadians(x, y)}):after(warshipLocation, 2, {x = x, y = y}):oncomplete(warshipMoveFinished)
 
     warshipLocation.moving = true
